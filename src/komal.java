4128e30b755322e7920e0d07139d41a946c2918f
test <harpreet@10.48.3.125>
2019-05-23 13:22:06 -0400

    commit 2

---
+++ src/komal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author harpreet
+ */
+public class komal {
+    
+    private int numofclasses;
+
+    /**
+     * Get the value of numofclasses
+     *
+     * @return the value of numofclasses
+     */
+    public int getNumofclasses() {
+        return numofclasses;
+    }
+
+    /**
+     * Set the value of numofclasses
+     *
+     * @param numofclasses new value of numofclasses
+     */
+    public void setNumofclasses(int numofclasses) {
+        this.numofclasses = numofclasses;
+    }
+
+}
