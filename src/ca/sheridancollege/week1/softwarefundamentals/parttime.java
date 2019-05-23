43e7a2211544bf0d792b5be952748a156784403e
test <harpreet@10.48.3.125>
2019-05-23 13:07:18 -0400

    commit 2

---
+++ src/ca/sheridancollege/week1/softwarefundamentals/parttime.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.week1.softwarefundamentals;
+
+/**
+ *
+ * @author harpreet
+ */
+public class parttime {
+    
+    private int numofclasses;
+
+    /**
+     * Get the value of numofclasses
+     *
+     * @return the value of numofclasses
+     */
+    public int getNumofclasses() {
+        return numofclasses;
+    }
+
+    /**
+     * Set the value of numofclasses
+     *
+     * @param numofclasses new value of numofclasses
+     */
+    public void setNumofclasses(int numofclasses) {
+        this.numofclasses = numofclasses;
+    }
+
+}
